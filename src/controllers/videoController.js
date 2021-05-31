6d6ebfff962c40396594fd4d03393eb0764219e3
ttac1 <81249031+ttac1@users.noreply.github.com>
2021-05-31 20:52:33 +0200

    edit video part

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,36 +1,46 @@
+let videos = [ 
+   {title : "First Video",
+    rating : 5,
+   commennt : 2,
+   createAt : "2 min ago",
+   views : 1,
+   id : 1
+   },
+   {title : "Hello2",
+    rating : 2,
+   commennt :14,
+   createAt : "1 min ago",
+   views : 432,
+   id : 2
+   },
+   {title : "Hello3",
+    rating : 4,
+   commennt : 3,
+   createAt : "1 min ago",
+   views : 123423,
+   id : 3
+   }
+];
 export const trending = (req, res) => {
-   const videos = [ 
-      {title : "Hello",
-       rating : 5,
-      commennt : 2,
-      createAt : "2 min ago",
-      views : 123,
-      id : 1
-      },
-      {title : "Hello2",
-       rating : 2,
-      commennt :14,
-      createAt : "1 min ago",
-      views : 432,
-      id : 2
-      },
-      {title : "Hello3",
-       rating : 4,
-      commennt : 3,
-      createAt : "1 min ago",
-      views : 123423,
-      id : 3
-      }
-   ];
    return res.render("home",{pageTitle : "Home", videos })
 }; 
-export const upload =(req,res) => res.send("Upload Video");
-export const see = (req,res) => res.render("watch", {pageTitle : "Watch"});
- export const edit = (req,res) => res.render("edit", {pageTitle : "Edit"});
- export const deleteVideo = (req,res) => {
-    return res.send("Delete Video");
- }; 
+export const watch = (req,res) => {
+   const { id } = req.params;
+   //const id = req.params.id; 윗 줄이랑 같음
+   const video = videos[id-1]
+   return res.render("watch",{pageTitle : `Watching: ${video.title}`, video});
+}; 
+ export const getEdit = (req,res) => {
+   const { id } = req.params;
+   const video = videos[id-1]
+   return res.render("edit", {pageTitle : `Editing: ${video.title}`, video});
+}; 
+export const postEdit = (req,res) => {
+   const { id } = req.params;
+   const { title }=req.body;
+   videos[id-1].title = title;
+   return res.redirect(`/videos/${id}`);
+};
 
-export const search = (req,res) => res.send("Search");
 
 
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,15 @@
 import express from "express";
-import {see,edit,upload,deleteVideo} from "../controllers/videoController"
+import {watch,upload,deleteVideo,getEdit,postEdit} from "../controllers/videoController"
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+//videoRouter.get("/:id(\\d+)/edit", getEdit);
+//videoRouter.post("/:id(\\d+)/edit", postEdit);
+//이 두줄을 합쳐놓은 것이 위의 한 줄
+
 
 
 export default videoRouter;
