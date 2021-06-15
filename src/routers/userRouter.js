8c423dac9d3e2941df2245c40eee9553a874c043
ttac1 <81249031+ttac1@users.noreply.github.com>
2021-06-15 20:49:51 +0200

    editProfile, changePassword

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getEdit, postEdit,logout, see, startGithubLogin, finishGithubLogin} from "../controllers/userController";
+import {getEdit, postEdit, getChangePassword, postChangePassword,logout, see, startGithubLogin, finishGithubLogin} from "../controllers/userController";
 import { protectorMiddleware } from "../middlewares";
 import { publicOnlyMiddleware } from "../middlewares";
 
@@ -8,6 +8,7 @@ const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
 userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter.route("/change-password").all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get("/:id", see);
